2cff0bc9a962f9d9922a61779b0588909c8780e9
matrixbirds <yooobuntu@gmail.com>
2019-07-08 15:16:39 +0800

    chore. update generate doc

--- typedoc.js
+++ typedoc.js
@@ -1,6 +1,7 @@
 const version = require("./package.json").version
 const fs = require("fs")
-fs.existsSync('latest') && fs.unlinkSync('latest')
+!fs.existsSync('latest') && fs.mkdirSync("latest")
+fs.writeFileSync('latest/index.html', `<html><head><title>agora react native rtm docs</title></head><body><script>window.location.href="https://agoraio.github.io/RN-SDK-RTM/${version}"</script></body></html>`);
 module.exports =
 {
     "mode": "file",
